fb9539c169fb03031aba5bb4644ffdce5155dd48
semjonsv <semjons.voronovs@inbox.lv>
2020-08-18 22:43:53 +0300

    NavBar component

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,29 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NavBar
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+$theme-colors: (
+        "primary": #3bbeff,
+        "danger": #fb3333
+);
+@import '../node_modules/bootstrap/scss/bootstrap';
+@import '../node_modules/bootstrap-vue/src/index.scss';
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-navbar type="light" class="shadow-sm">
+    <img class="ml-2 ml-sm-5" src="../assets/logo.png" alt="Logo" width="30px">
+    <b-navbar-brand class="ml-1 d-none d-sm-block" tag="h1" href="#">
+      Name
+    </b-navbar-brand>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-button variant="light" class="mr-3">Login</b-button>
+          <b-button variant="danger" class="mr-2 mr-sm-5">Register</b-button>
+        </b-nav-form>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
